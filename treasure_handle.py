533c0fbd1e3c24673697ade2d6a0c5a11a8b5bf8
Ivan <lubovdobro@gmail.com>
2023-04-07 23:51:09 +0400

    update working

--- treasure_handle.py
+++ treasure_handle.py
@@ -1,39 +1,75 @@
 import pytesseract
-from typing import Dict
 from PIL import Image
-import aiogram
-from aiogram.types import Message
+from aiogram import types, Bot
 import re
 import asyncio
+import os
 
 pattern = re.compile(r'\b\d+\.\d{5}\b')
-bot: aiogram.Bot
+bot: Bot
 
-async def treasure_handle(message: Message) -> None:
+async def treasure_handle(message: types.Message) -> None:
     photo: str = message.photo[-1].file_id
     file = await bot.download_file_by_id(photo)
     result = find_coordinates(file)
     
-    format_latitude = result[0].replace(".",r"\.")
-    format_longitude = result[1].replace(".",r"\.")
-    text_replay = await message.answer(text=f'Coordinates for {format_latitude}, {format_longitude}')
-    map_replay = await message.answer_location(latitude=result[0], longitude=result[1])
+    if len(result) == 2:
+        succese_answer(message, result)
+    else:
+        sorry_answer(message)
     
-    await asyncio.sleep(60)
+    if os.path.exists(file.name):
+        os.remove(file.name)
+    else:
+        print(f"File '{file.name}' does not exist")
 
-    await message.delete()
-    await map_replay.delete()
-    await text_replay.delete()
 
-
-async def treasure_rejecte(message: Message) -> None:
+async def treasure_rejecte(message: types.Message) -> None:
     await message.answer("Send picture, bro!")
 
 
 def find_coordinates(file) -> set:
     img = Image.open(file)
-    img = img.convert('L')
-    text: str = pytesseract.image_to_string(img)
-    result = re.findall(pattern, text)
     
-    return result
+    coord = parse_image(img)
+    if len(coord) != 2:
+        coord = parse_image(img, True)
+    
+    return coord
+
+
+def parse_image(img, invert=False) ->set:
+    if invert:
+        img = img.convert('L')
+    
+    text: str = pytesseract.image_to_string(img)
+    return re.findall(pattern, text)
+
+
+def format_coordinates(coord) ->str:
+    return  coord[0].replace(".",r"\.") + coord[1].replace(".",r"\.")
+
+
+async def sorry_answer(message: types.Message):
+    message.answer(text='Sorry, can`t find coordinates')
+    eat_breadcrumbs(message)
+
+
+async def succese_answer(message: types.Message, coord):
+    text_format = format_coordinates(coord)
+    text_replay = await message.answer(text=f'Coordinates for {text_format}')
+    map_replay = await message.answer_location(latitude=coord[0], longitude=coord[1])
+    
+    eat_breadcrumbs(text_format, text_replay, map_replay)
+
+
+async def eat_breadcrumbs(message, replay_message=None, replay_map=None):
+    await asyncio.sleep(60)
+
+    try:
+        await message.delete()
+        await replay_map.delete()
+        await replay_message.delete()
+    except TypeError:
+        print("Caught TypeError: Cannot call my_method on None object")
+    
