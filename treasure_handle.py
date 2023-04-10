f7adcf99a51e54cac405ab01c9a22e4f15a4b706
Ivan <lubovdobro@gmail.com>
2023-04-10 23:23:18 +0400

    update performance

--- treasure_handle.py
+++ treasure_handle.py
@@ -3,28 +3,28 @@ from PIL import Image
 from aiogram import types, Bot
 import re
 import asyncio
-import os
+import logging
 
 pattern = re.compile(r'\b\d+\.\d{5}\b')
 bot: Bot
 
+logging.basicConfig(level=logging.INFO)
+
 async def treasure_handle(message: types.Message) -> None:
+    logging.info(f'treasure_handle() for uset {message.from_user.id}')
+    
     photo: str = message.photo[-1].file_id
     file = await bot.download_file_by_id(photo)
     result = find_coordinates(file)
     
     if len(result) == 2:
-        succese_answer(message, result)
+        await succese_answer(message, result, file)
     else:
-        sorry_answer(message)
+        await sorry_answer(message, file)
     
-    if os.path.exists(file.name):
-        os.remove(file.name)
-    else:
-        print(f"File '{file.name}' does not exist")
-
 
 async def treasure_rejecte(message: types.Message) -> None:
+    logging.info(f'treasure_rejecte() for uset {message.from_user.id}')
     await message.answer("Send picture, bro!")
 
 
@@ -50,17 +50,22 @@ def format_coordinates(coord) ->str:
     return  coord[0].replace(".",r"\.") + coord[1].replace(".",r"\.")
 
 
-async def sorry_answer(message: types.Message):
-    message.answer(text='Sorry, can`t find coordinates')
-    eat_breadcrumbs(message)
+async def sorry_answer(message: types.Message, file):
+    
+    # logging.info(f'File saved in {file.name}')
+    
+    await message.answer(text='Sorry, can`t find coordinates')
+    await eat_breadcrumbs(message)
 
 
-async def succese_answer(message: types.Message, coord):
+async def succese_answer(message: types.Message, coord, file):
+    del file
+      
     text_format = format_coordinates(coord)
     text_replay = await message.answer(text=f'Coordinates for {text_format}')
     map_replay = await message.answer_location(latitude=coord[0], longitude=coord[1])
     
-    eat_breadcrumbs(text_format, text_replay, map_replay)
+    await eat_breadcrumbs(text_format, text_replay, map_replay)
 
 
 async def eat_breadcrumbs(message, replay_message=None, replay_map=None):
@@ -72,4 +77,6 @@ async def eat_breadcrumbs(message, replay_message=None, replay_map=None):
         await replay_message.delete()
     except TypeError:
         print("Caught TypeError: Cannot call my_method on None object")
+        
+    
     
